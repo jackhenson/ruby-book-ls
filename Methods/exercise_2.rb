46cc72f433fee622540d28d0c73e6952a1724c41
Jack Henson <jackhensonmedia@gmail.com>
2021-10-24 18:42:50 +0000

    Added exercises from Methods

---
+++ Methods/exercise_2.rb
@@ -0,0 +1,14 @@
+1. x = 2
+#returns 2
+
+2. puts x = 2
+#returns nil, puts returns nil
+
+3. p name = "Joe"
+#returns "Joe", p returns argument
+
+4. four = "four"
+#returns "four"
+
+5. print something = "nothing"
+#returns nil, print returns nil
