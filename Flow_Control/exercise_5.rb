4aff821e3bb5a76810f5610da2d865aaf63d742d
Jack Henson <jackhensonmedia@gmail.com>
2021-10-26 22:51:20 +0000

    Added Flow Control exercises

---
+++ Flow_Control/exercise_5.rb
@@ -0,0 +1,11 @@
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
+
+#add additional end on line 7 to close off method
---
+++ Flow_Control/exercise_6.rb
@@ -0,0 +1,17 @@
+(32 * 4) >= "129"
+#error, comparing string to integer
+
+847 == '874'
+#false
+
+'847' < '846'
+#false
+
+'847' > '846'
+#true
+
+'847' > '8478'
+#false
+
+'847' < '8478'
+#true
